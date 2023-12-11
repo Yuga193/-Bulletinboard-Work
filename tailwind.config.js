0fe243a001da35f1ea7b1ac54bd50b13c3e0ebe2
{seal} <{130330313+NABEseal@users.noreply.github.com}>
2023-12-11 12:20:33 +0900

    css 反映

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/templates/**/*.html", "./app/static/src/**/*.js"],
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/src/**/*.js"
+  ],
   theme: {},
   plugins: [],
-};
+}
